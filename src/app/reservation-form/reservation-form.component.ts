73fa3a89585d68bea4f477bcc22209081792c12e
HIBA <ibrahimih.hiba@gmail.com>
2026-01-15 01:37:35 +0100

    Add observable & Mock Apis

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -37,10 +37,12 @@ export class ReservationFormComponent implements OnInit {
     let id = this.activatedRoute.snapshot.paramMap.get('id')
 
     if(id){
-      let reservation = this.reservationService.getReservation(id)
-
-      if(reservation)
+      this.reservationService.getReservation(id).subscribe(reservation => {
+        if(reservation)
         this.reservationForm.patchValue(reservation)
+      })
+
+
     }
   }
   
@@ -53,11 +55,14 @@ export class ReservationFormComponent implements OnInit {
 
       if(id){
         // Update
-        this.reservationService.updateReservation(id, reservation)
+        this.reservationService.updateReservation(id, reservation).subscribe(()=>{
+          console.log("Update request processed")
+        })
       } else {
         // New
-        this.reservationService.addReservation(reservation)   
-
+        this.reservationService.addReservation(reservation).subscribe(()=>{
+          console.log("Add request processed")
+        })
       }
 
       this.router.navigate(['/list'])
